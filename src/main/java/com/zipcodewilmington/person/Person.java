1eaf17e07015e26877222cbd23696bcc705b8aed
christine <christine@zipcodes-MacBook-Pro-10.local>
2020-11-08 00:19:11 -0500

    created 5 features with tests for each, passed all tests

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,9 +6,21 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private int feetTall;
+    private int numOfToes;
+    private int numOfEars;
+    private int fingernails;
+    private int scars;
+
 
     public Person() {
-
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
+        //this.feetTall
+        //this.numOfToes
+        //this.numOfEars
+        //this.fingernails
+        //this.scars
     }
 
     public Person(int age) {
@@ -28,15 +40,55 @@ public class Person {
         this.name = name;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public String getName() {
         return name;
     }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public Integer getAge() {
         return age;
+    }
+
+    public void setFeetTall(Integer feetTall) {
+        this.feetTall = feetTall;
+    }
+
+    public Integer getFeetTall() {
+        return feetTall;
+    }
+
+    public void setNumOfToes(Integer numOfToes) {
+        this.numOfToes = numOfToes;
+    }
+
+    public Integer getNumOfToes() {
+        return numOfToes;
+    }
+
+    public void setNumOfEars(Integer numOfEars) {
+        this.numOfEars = numOfEars;
+    }
+
+    public Integer getNumOfEars() {
+        return numOfEars;
+    }
+
+    public void setFingernails(Integer fingernails) {
+        this.fingernails = fingernails;
+    }
+
+    public Integer getFingernails() {
+        return fingernails;
+    }
+
+    public void setScars(Integer scars) {
+        this.scars = scars;
+    }
+
+    public Integer getScars() {
+        return scars;
     }
 }
